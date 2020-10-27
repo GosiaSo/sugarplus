07502ccc1a36cd666cac11dfce2bcdcff761a7a9
malgorzatasobocinska <sobocinska.malgo@gmail.com>
2020-10-27 12:43:55 +0100

    deleted .jsp files. creating another angular app

--- src/main/java/pl/sobocinska/sugarplus/sugar/SugarController.java
+++ src/main/java/pl/sobocinska/sugarplus/sugar/SugarController.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.sobocinska.sugarplus.users.JpaUserService;
 
